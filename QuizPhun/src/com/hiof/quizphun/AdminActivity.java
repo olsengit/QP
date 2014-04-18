1c4e4b61b234d5ebb537f709c510858e37e4c382
Martin <Martin@10.0.0.4>
2014-04-18 20:32:42 +0200

    Change-Id: I84e18c3caf8d5b3c42e58392358771a6971e5b51

--- QuizPhun/src/com/hiof/quizphun/AdminActivity.java
+++ QuizPhun/src/com/hiof/quizphun/AdminActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-
+ 
 public class AdminActivity extends ActionBarActivity {
 
 	@Override
