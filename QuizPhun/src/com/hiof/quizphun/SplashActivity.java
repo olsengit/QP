c054a3357549016c9e0ac105d4d0a89b083e2715
Patrick <patricmj@hotmail.com>
2014-04-22 00:35:43 +0200

    Bugfiks - splashscreen

--- QuizPhun/src/com/hiof/quizphun/SplashActivity.java
+++ QuizPhun/src/com/hiof/quizphun/SplashActivity.java
@@ -4,26 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
 
 public class SplashActivity extends Activity {
 
     private final int SPLASH_DISPLAY_LENGHT = 3000;
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.splash_screen);
 
-        /* New Handler to start the Menu-Activity 
-         * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-            	//Intent i = new Intent(this, CategoryActivity.class);
-        		//startActivity(i);
                 Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(mainIntent);
                 finish();
